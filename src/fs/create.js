04f6f54205b80954436143378f9ac9d388a6181f
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-06-24 15:11:43 +0300

    feat: fs:create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-  // Write your code here
+import { writeFile } from 'fs/promises';
+
+const file = {
+  path: 'src/fs/files/',
+  name: 'fresh.txt',
+  content: 'I am fresh and young',
 };
 
-await create();
+const create = async ({ path, name, content }) => {
+  try {
+    await writeFile(path.concat(name), content, {
+      flag: 'wx',
+    });
+  } catch {
+    throw Error('FS operation failed');
+  }
+};
+
+await create(file);
