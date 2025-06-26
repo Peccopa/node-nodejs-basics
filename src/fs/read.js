afa77a009088f6d092d4fcf3e91e5e195f69a951
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-06-26 09:23:51 +0300

    feat: add fs:read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
-const read = async () => {
-  // Write your code here
+import { readFile } from 'fs/promises';
+
+const filePath = 'src/fs/files/fileToRead.txt';
+
+const read = async (filePath) => {
+  try {
+    const fileContent = await readFile(filePath, { encoding: 'utf-8' });
+    console.log(fileContent);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
-await read();
+await read(filePath);
